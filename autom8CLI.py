dbe43c8510c7e653ce997ced611cee852c6fd99b
Jordan Bradshaw <jordanbradshaw27@aol.com>
2021-01-26 18:18:34 -0800

    Adding Flask still unorganized

--- autom8CLI.py
+++ autom8CLI.py
@@ -6,7 +6,7 @@ import yahoo_fantasy_api as yfa
 
 from addons import connectionManager, leagueManager
 
-menu = ['Roster', 'Matchup', 'Players', 'League']
+menu = ["Roster", "Matchup", "Players", "League"]
 
 
 def printMenu(stdscr, menuWords: List[str], h, w):
@@ -23,9 +23,9 @@ def printMenu(stdscr, menuWords: List[str], h, w):
 def printRoster(stdscr, h, w):
     def formatRoster(stdscr, player, h, w):
         x = (w // 10) * 3
-        stdscr.addstr(3 + h, x, player['selected_position'])
-        x = ((w // 4) * 2) - (len(player['name']) // 2)
-        stdscr.addstr(3 + h, x, player['name'])
+        stdscr.addstr(3 + h, x, player["selected_position"])
+        x = ((w // 4) * 2) - (len(player["name"]) // 2)
+        stdscr.addstr(3 + h, x, player["name"])
 
     printMenu(stdscr, ["", menu[0], menu[1]], h, w)
     todaysRoster = globalLeague.getTodaysRoster()
@@ -34,19 +34,47 @@ def printRoster(stdscr, h, w):
         formatRoster(stdscr, player, index, w)
 
     stdscr.refresh()
-
+#Hello
 
 def printMatchup(stdscr, h, w):
     def formatMatchup(stdscr, player, h, w):
         x = (w // 10) * 3
-        stdscr.addstr(3 + h, x, player['selected_position'])
-        x = ((w // 4) * 2) - (len(player['name']) // 2)
-        stdscr.addstr(3 + h, x, player['name'])
+        stdscr.addstr(3 + h, x, player["selected_position"])
+        x = ((w // 4) * 2) - (len(player["name"]) // 2)
+        stdscr.addstr(3 + h, x, player["name"])
 
     printMenu(stdscr, [menu[0], menu[1], menu[2]], h, w)
     todaysMatchup = globalLeague.getWeeksMatchup()
-    #print(todaysMatchup)
-    #for index, player in enumerate(todaysRoster):
+    stdscr.refresh()
+
+
+def printPlayers(stdscr, h, w):
+    def formatPlayers(stdscr, h, w, key, items):
+        positions = ["PG", "SG", "SF", "PF", "C"]
+        x = ((w // 6) * (positions.index(key) + 1)) - (len(key) // 2)
+        stdscr.addstr(3, x, key)
+        for index, item in enumerate(items, start=4):
+            x = ((w // 6) * (positions.index(key) + 1)) - (len(item["name"]) // 2)
+            stdscr.addstr(index, x, item["name"])
+
+    printMenu(stdscr, [menu[1], menu[2], menu[3]], h, w)
+    topFAs = globalLeague.getTopFreeAgents()
+    for key, item in topFAs.items():
+        formatPlayers(stdscr, h, w, key, item)
+    stdscr.refresh()
+
+
+def printLeague(stdscr, h, w):
+    def formatMatchup(stdscr, player, h, w):
+        x = (w // 10) * 3
+        stdscr.addstr(3 + h, x, player["selected_position"])
+        x = ((w // 4) * 2) - (len(player["name"]) // 2)
+        stdscr.addstr(3 + h, x, player["name"])
+
+    printMenu(stdscr, [menu[2], menu[3], ""], h, w)
+    todaysMatchup = globalLeague.getWeeksMatchup()
+    # print(todaysMatchup)
+    # for index, player in enumerate(todaysRoster):
     #    formatMatchup(stdscr, player, index, w)
     #
     stdscr.refresh()
@@ -63,7 +91,7 @@ def commandLineInterface(stdscr):
         key = stdscr.getch()
 
         stdscr.clear()
-        #printMenu(stdscr, selectedColumnIndex)
+        # printMenu(stdscr, selectedColumnIndex)
         if key == curses.KEY_LEFT and selectedColumnIndex > 0:
             selectedColumnIndex -= 1
         elif key == curses.KEY_RIGHT and selectedColumnIndex < len(menu) - 1:
@@ -78,55 +106,58 @@ def activeMenu(stdscr, selectedColumnIndex):
         printRoster(stdscr, h, w)
     if selectedColumnIndex == 1:
         printMatchup(stdscr, h, w)
+    if selectedColumnIndex == 2:
+        printPlayers(stdscr, h, w)
+    if selectedColumnIndex == 3:
+        printLeague(stdscr, h, w)
 
 
 def getLeague():
     def getLeagueName(passedLeagueValue):
-        return yfa.League(oauth, passedLeagueValue).settings()['name']
+        return yfa.League(oauth, passedLeagueValue).settings()["name"]
 
     # Setup up connection to yahoo and select NBA
     oauth = connectionManager.connectionManager()
-    sport = yfa.Game(oauth, 'nba')
+    sport = yfa.Game(oauth, "nba")
 
     def chooseLeague(possibleChoices):
-        print('Choose from the following League IDs:')
-        print('----------------------------------')
+        print("Choose from the following League IDs:")
+        print("----------------------------------")
         for index, league in enumerate(possibleLeagues):
             leagueName = getLeagueName(league)
-            print(
-                f'Index: {index} League Name: {leagueName} League ID: {league}'
-            )
-        print('----------------------------------')
+            print(f"Index: {index} League Name: {leagueName} League ID: {league}")
+        print("----------------------------------")
         while True:
-            returnLeague = input(
-                "Input the index you're selecting or type -1 to exit: ")
+            returnLeague = input("Input the index you're selecting or type -1 to exit: ")
             try:
-                if returnLeague == '-1':
-                    print('Exiting...')
+                if returnLeague == "-1":
+                    print("Exiting...")
                     exit()
-                elif int(returnLeague) > -1 and int(
-                        returnLeague) <= possibleChoices:
+                elif int(returnLeague) > -1 and int(returnLeague) <= possibleChoices:
                     return int(returnLeague)
                     break
                 else:
-                    print('-Error: Invalid Index!-')
+                    print("-Error: Invalid Index!-")
             except ValueError:
-                print('-Error: Invalid Input!-')
+                print("-Error: Invalid Input!-")
 
     possibleLeagues = sport.league_ids(year=2020)
     leagueIndex = chooseLeague(len(possibleLeagues) - 1)
     currentLeague = sport.to_league(possibleLeagues[leagueIndex])
     roster = leagueManager.leagueManager(currentLeague).getTodaysRoster()
     matchups = leagueManager.leagueManager(currentLeague).getWeeksMatchup()
-    #print(roster)
-    #print(matchups)
+    # print(roster)
+    # print(matchups)
     for index, item in enumerate(
-            matchups['fantasy_content']['league'][1]['scoreboard']['0']
-        ['matchups']['0']['matchup']['status']):
+        matchups["fantasy_content"]["league"][1]["scoreboard"]["0"]["matchups"]["0"]["matchup"]["status"]
+    ):
         print(index, item)
-        print(' ')
+        print(" ")
 
-    #exit()
+    # for item, key in pgs.items():
+    #    print(key)
+    # print(pgs)
+    # exit()
     return leagueManager.leagueManager(currentLeague)
     # print(currentLeague.matchups())
     temp.getTodaysRoster()
@@ -137,5 +168,5 @@ def getLeague():
 
 globalLeague = getLeague()
 curses.wrapper(commandLineInterface)
-#if __name__ == "__main__":
+# if __name__ == "__main__":
 #    cli()
